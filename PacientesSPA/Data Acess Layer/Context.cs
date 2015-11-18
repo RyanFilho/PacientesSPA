0d0af6dbc34c98a85c1aaf60e6e4d4f9151a5d8c
Ryan Sampaio Filho <ryanfilhoryanfilho@gmail.com>
2015-11-18 17:03:48 -0300

    Configuração do Entity Framework e Context

---
+++ PacientesSPA/Data Acess Layer/Context.cs
@@ -0,0 +1,14 @@
+﻿using PacientesSPA.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PacientesSPA.Data_Acess_Layer
+{
+    public class Context : DbContext
+    {
+        public DbSet<Paciente> Pacientes { get; set; }
+    }
+}
